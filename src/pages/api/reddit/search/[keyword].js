e4c540f2909f1463147b3e69cb58ef7e4b22d54d
Hardik Malhotra <hardikmalhotra2000@gmail.com>
2024-04-26 22:56:51 +0530

    Update [keyword].js

--- src/pages/api/reddit/search/[keyword].js
+++ src/pages/api/reddit/search/[keyword].js
@@ -1,6 +1,18 @@
-import fetch from 'isomorphic-fetch';
+import axios from "axios";
 
 export default async function handler(req, res) {
-  const { keyword } = req.query;
-  res.status(200).json(keyword);
+    const { keyword } = req.query;
+    try {
+        const searchRedditResponse = await axios.get(`https://www.reddit.com/search.json?q=${keyword}&type=sr`,{
+            headers: {
+                "User-Agent" : "Mozilla/5.0 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+                "Referer" : "https://thepulped.vercel.app/dashboard/create-audience"
+            }
+        }
+    );
+        const searchRedditResult = searchRedditResponse.data;
+        res.status(200).json(searchRedditResult);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }
