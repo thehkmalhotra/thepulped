4495d5516c4da616068d50375846c2b923973107
Hardik Malhotra <hardikmalhotra2000@gmail.com>
2024-04-26 16:57:18 +0530

    Update index.js

--- src/pages/api/reddit/search/index.js
+++ src/pages/api/reddit/search/index.js
@@ -1,15 +1,12 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     const { keyword } = req.body;
     const { type } = req.body;
     const { sort } = req.body;
     try {
-        const searchRedditResponse = await axios.get(`https://www.reddit.com/search.json?q=${keyword}&type=${type}&sort=${sort}`,{
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-            }
-        });
+        const searchRedditResponse = await axios.get(`https://www.reddit.com/search.json?q=${keyword}&type=${type}&sort=${sort}`);
         const searchRedditResult = searchRedditResponse.data;
         res.status(200).json(searchRedditResult);
     } catch (error) {
