ebfea06fbf9fdc0bb83f9a35b69f12ca94ef19d5
Hardik Malhotra <hardikmalhotra2000@gmail.com>
2024-04-26 16:26:11 +0530

    26th April

--- src/pages/api/reddit/search/index.js
+++ src/pages/api/reddit/search/index.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-    const { keyword } = req.query;
+    const { keyword } = req.body;
+    const { type } = req.body;
+    const { sort } = req.body;
     try {
-        const searchRedditResponse = await axios.get(`https://www.reddit.com/search.json?q=${keyword}&type=sr&sort=relevance`);
+        const searchRedditResponse = await axios.get(`https://www.reddit.com/search.json?q=${keyword}&type=${type}&sort=${sort}`);
         const searchRedditResult = searchRedditResponse.data;
         res.status(200).json(searchRedditResult);
     } catch (error) {
