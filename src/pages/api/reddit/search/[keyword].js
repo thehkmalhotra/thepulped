861f56258089849b1d784d93b099a8243d11e8c8
Hardik Malhotra <hardikmalhotra2000@gmail.com>
2024-04-26 22:48:28 +0530

    Update [keyword].js

--- src/pages/api/reddit/search/[keyword].js
+++ src/pages/api/reddit/search/[keyword].js
@@ -4,5 +4,5 @@ export default async function handler(req, res) {
   const { keyword } = req.query;
   const response = await fetch(`https://www.reddit.com/search.json?q=${encodeURIComponent(keyword)}&type=sr&sort=relevance`);
   const data = await response.json();
-  res.status(500).json(data);
+  res.status(200).json(data);
 }
