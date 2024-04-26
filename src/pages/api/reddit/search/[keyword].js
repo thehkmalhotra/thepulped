bd1b565547cc20b27fafa484cbb9b3aa9887b22c
Hardik Malhotra <hardikmalhotra2000@gmail.com>
2024-04-26 22:47:37 +0530

    Update [keyword].js

--- src/pages/api/reddit/search/[keyword].js
+++ src/pages/api/reddit/search/[keyword].js
@@ -2,16 +2,7 @@ import fetch from 'isomorphic-fetch';
 
 export default async function handler(req, res) {
   const { keyword } = req.query;
-
-  try {
-    const response = await fetch(`https://www.reddit.com/search.json?q=${encodeURIComponent(keyword)}&type=sr&sort=relevance`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch Reddit data');
-    }
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (error) {
-    console.error('Failed to fetch Reddit data:', error.message);
-    res.status(500).json({ error: 'Failed to fetch Reddit data' });
-  }
+  const response = await fetch(`https://www.reddit.com/search.json?q=${encodeURIComponent(keyword)}&type=sr&sort=relevance`);
+  const data = await response.json();
+  res.status(500).json(data);
 }
