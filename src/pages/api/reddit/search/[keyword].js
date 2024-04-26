c28d801b48ce7f3c563252cc85fd705c023e8441
Hardik Malhotra <hardikmalhotra2000@gmail.com>
2024-04-26 22:43:20 +0530

    26th April

--- src/pages/api/reddit/search/[keyword].js
+++ src/pages/api/reddit/search/[keyword].js
@@ -1,12 +1,17 @@
-import axios from "axios";
+import fetch from 'isomorphic-fetch';
 
 export default async function handler(req, res) {
-    const { keyword } = req.query;
-    try {
-        const searchRedditResponse = await axios.get(`https://www.reddit.com/search.json?q=${keyword}&type=sr`);
-        const searchRedditResult = searchRedditResponse.data;
-        res.status(200).json(searchRedditResult);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+  const { keyword } = req.query;
+
+  try {
+    const response = await fetch(`https://www.reddit.com/search.json?q=${encodeURIComponent(keyword)}&type=sr&sort=relevance`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch Reddit data');
     }
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Failed to fetch Reddit data:', error.message);
+    res.status(500).json({ error: 'Failed to fetch Reddit data' });
+  }
 }
